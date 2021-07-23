539637bcb1a7c3c756a23cf301079e38ff255583
Hpollard003 <hpollard003@gmail.com>
2021-07-23 15:45:23 -0400

    commit tree d6388c9ddd9da8ef7f8ae5a8913d8e22f9c57024

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+    array.sort
+end
+ 
+def sort_array_desc(array)
+    # array.sort.reverse
+    array.sort do |left, right|
+        right <=> left
+    end
+end
+ 
+def sort_array_char_count(array)
+    array.sort do |left, right|
+        left.length <=> right.length
+    end
+end
+ 
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+ 
+def reverse_array(array)
+    array.reverse
+end
+ 
+def kesha_maker(array)
+    array.each do |name|
+        name[2] = "$"
+    end
+end
+ 
+def find_a(array)
+    array.filter do |string|
+        string[0] == "a"
+    end
+end
+ 
+def sum_array(array)
+    array.inject(:+)
+end
+ 
+def add_s(array)
+    array.map.with_index do |name, index|
+        if index == 1
+            name
+        else
+            name + "s"
+        end
+    end
+    
+end
+ 
 
